7fe0fc8800d383cb248b3a18b929c75d97b42717
Etienne Roulet <etienne.roulet@outlook.com>
2023-10-04 22:45:29 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.cmd([[colorscheme onedark_dark]])
+vim.o.cursorline = true
+vim.cmd [[highlight CursorLine guibg=#272221 ctermbg=124]]
